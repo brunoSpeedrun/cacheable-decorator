5eea77802d0208525fe106c8de39f484e79a42f4
bxmoura <bruno.xavier.moura@gmail.com>
2025-06-21 22:57:11 -0300

    refactor(cache-manager): should be enabled by default

--- lib/cache/cache-manager-2.ts
+++ lib/cache/cache-manager-2.ts
@@ -1,6 +1,21 @@
+export type CacheManagerOptions = {
+  enabled?: boolean;
+};
+
 const INSTANCE = Symbol('INSTANCE');
+const ENABLED = Symbol('ENABLED');
 
 export class CacheManager {
+  private [ENABLED] = true;
+
+  get isEnabled() {
+    return this[ENABLED];
+  }
+
+  initialize(options: CacheManagerOptions) {
+    throw new Error('Method not implemented.');
+  }
+
   private static [INSTANCE]: CacheManager;
 
   static getInstance() {
--- test/cache-manager-2.spec.ts
+++ test/cache-manager-2.spec.ts
@@ -1,6 +1,8 @@
 import { CacheManager } from '../lib/cache/cache-manager-2';
 
 describe('CacheManager', () => {
+  const cacheManager = CacheManager.getInstance();
+
   it('should be singleton', () => {
     const cacheManagerOne = CacheManager.getInstance();
     const cacheManagerTwo = CacheManager.getInstance();
@@ -8,5 +10,9 @@ describe('CacheManager', () => {
     expect(cacheManagerOne).toBeDefined();
     expect(cacheManagerTwo).toBeDefined();
     expect(cacheManagerOne).toBe(cacheManagerTwo);
+  });
+
+  it('should be enabled by default', () => {
+    expect(cacheManager.isEnabled).toBe(true);
   });
 });
